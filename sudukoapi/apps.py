e5cf6d8cc279d92958c674d9a18976f1affb421f
Dov Davidovics <dov@medadoc.com>
2022-06-19 23:34:14 -0700

    copy the app , urls and views from previous project

--- sudukoapi/apps.py
+++ sudukoapi/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class SudukoapiConfig(AppConfig):
+class SudukobackendConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'sudukoapi'
+    name = 'sudukoBackEnd'
--- sudukoapi/views.py
+++ sudukoapi/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+
+from rest_framework.decorators import api_view
+from rest_framework.response  import Response
+
+def say_hello(request):
+    
+    return HttpResponse("Hello World")
+@api_view()
+def puzzles(request):
+    puzList = [
+       {"id": "puzzleID1",
+        "puzzleString": "010020300004005060070000008006900070000100002030048000500006040000800106008000000"},
+       {"id": "p2",
+        "puzzleString": "000123000040050060000000000100000003260070081900000002000000000080060040000294000"},
+       {"id": "p3",
+        "puzzleString": "ZZZZ23000040050060000000000100000003260070081900000002000000000080060040000294000"},
+       {"id": "p4",
+        "puzzleString": "1111123000040050060000000000100000003260070081900000002000000000080060040000294000"}
+       
+    ]
+    return Response(puzList)
+
+
